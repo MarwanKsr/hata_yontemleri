20e169338ea487ef03e859efd1c8b953b8210f32
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-08 20:48:12 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace hata_tontemleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {try
+        {   Console.WriteLine("Birinci sayi giriniz:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ikinci sayi giriniz:");
+            int b = int.Parse(Console.ReadLine());
+            int c = a+b ;
+            Console.WriteLine("Toplam: " + c);
+            Console.WriteLine("Toplama islemi tamamlandi");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("girmis oldugunuz deger bir sayi degil");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Yeniden denemek için program yeniden baslatabilirsiniz");
+        }
+        finally
+        {
+            Console.WriteLine("Programi kullandiginiz icin tesekkur ederiz");
+        }
+        }
+    }
+}
